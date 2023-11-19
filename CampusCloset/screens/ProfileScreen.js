ce0f1124469cde129d87cfce0ff25b7a521b3186
Risha Narayanan <rishasn123@gmail.com>
2023-11-18 19:38:27 -0500

    hard-coded data displayed in profile/cart screen

--- CampusCloset/screens/ProfileScreen.js
+++ CampusCloset/screens/ProfileScreen.js
@@ -5,14 +5,24 @@ import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { launchImageLibrary } from 'react-native-image-picker';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
+const itemsMockBought = [
+    { id: '1', name: 'Tailgate T-Shirt', price: 20, quantity: 1 },
+    { id: '2', name: 'Vintage College Cap', price: 15, quantity: 1 },
+];
+
+const itemsMockSold = [
+    { id: '1', name: 'Tailgate T-Shirt', price: 20, quantity: 1 },
+    { id: '2', name: 'Vintage College Cap', price: 15, quantity: 1 },
+];
+
 const ProfileScreen = () => {
     const [userInfo, setUserInfo] = useState({
-        name: '',
-        email: '',
-        itemsBought: 0,
-        itemsSold: 0,
-        profilePic: '',
+        name: 'Netra',
+        email: 'netraj@netraj.edu',
+        profilePic: ''
     });
+    const [itemsBought, setItemsBought] = useState(itemsMockBought);
+    const [itemsSold, setItemsSold] = useState(itemsMockSold);
 
     useEffect(() => {
         const getUserInfo = async () => {
@@ -22,11 +32,17 @@ const ProfileScreen = () => {
                     const data = userDoc.data();
                     setUserInfo(prevState => ({
                         ...prevState,
-                        name: data.name,
-                        email: data.email,
-                        itemsBought: data.itemsBought,
-                        itemsSold: data.itemsSold,
-                        profilePic: data.profilePic || '',
+                        name: userInfo.name,
+                        email: userInfo.email,
+                        profilePic: userInfo.profilePic || '',
+                    }));
+                    setItemsBought(prevState => ({
+                        ...prevState,
+                        itemsBought: itemsBought
+                    }));
+                    setItemsSold(prevState => ({
+                        ...prevState,
+                        itemsSold: itemsSold
                     }));
                 } else {
                     console.log("No such document!");
@@ -38,6 +54,17 @@ const ProfileScreen = () => {
 
         getUserInfo();
     }, []);
+    
+    const renderItem = (items) => {
+        return items.map(item => (
+            <View style={styles.itemContainer}>
+                <Text key={item.id} style={styles.itemInfo}>
+                {item.name} ~ Price: ${item.price} ~ Quantity: {item.quantity}
+                </Text>
+            </View>
+        ));
+    };
+    
 
     const handleSelectProfilePic = () => {
         launchImageLibrary({ mediaType: 'photo' }, (response) => {
@@ -66,17 +93,18 @@ const ProfileScreen = () => {
 
     return (
         <View style={styles.container}>
+            <Text style={styles.header}>{userInfo.name}</Text>
             <TouchableOpacity onPress={handleSelectProfilePic}>
                 <Image
-                    source={userInfo.profilePic ? { uri: userInfo.profilePic } : require('../assets/favicon.png')} 
+                    source={userInfo.profilePic ? { uri: userInfo.profilePic } : require('../assets/default-profile-pic.jpeg')} 
                     style={styles.profilePic}
                 />
             </TouchableOpacity>
-            <Text style={styles.header}>Profile</Text>
-            <Text style={styles.info}>Name: {userInfo.name}</Text>
-            <Text style={styles.info}>Email: {userInfo.email}</Text>
-            <Text style={styles.info}>Items Bought: {userInfo.itemsBought}</Text>
-            <Text style={styles.info}>Items Sold: {userInfo.itemsSold}</Text>
+            <Text style={styles.email}>email: {userInfo.email}</Text>
+            <Text style={styles.info}>Items Bought:</Text>
+            <Text style={styles.itemContainer}>{renderItem(itemsBought)}</Text>
+            <Text style={styles.info}>Items Sold:</Text>
+            <Text style={styles.itemContainer}>{renderItem(itemsSold)}</Text>
         </View>
     );
 };
@@ -84,7 +112,7 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'top',
         alignItems: 'center',
         padding: 20,
     },
@@ -92,16 +120,24 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         borderRadius: 50,
-        marginBottom: 20,
+        marginVertical: 0.2,
     },
     header: {
-        fontSize: 24,
+        fontSize: 40,
         marginBottom: 20,
+    },
+    email: {
+        fontSize: 15,
+        marginVertical: 20,
     },
     info: {
         fontSize: 18,
-        marginVertical: 5,
+        marginVertical: 10,
     },
+    itemContainer: {
+        fontSize: 15,
+        marginVertical: 2,
+    }
 });
 
 export default ProfileScreen;
